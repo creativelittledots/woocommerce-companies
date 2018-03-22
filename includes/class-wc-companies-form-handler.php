0ec57167ddb493813ec2d643dfb000c69b9e6953
terence1990 <terence@creativelittledots.co.uk>
2018-03-22 20:22:34 +0000

    fixes

--- includes/class-wc-companies-form-handler.php
+++ includes/class-wc-companies-form-handler.php
@@ -20,6 +20,9 @@ class WC_Companies_Form_Handler extends WC_Form_Handler {
 	 */
 	public function __construct() {
 		
+		add_filter( 'woocommerce_default_address_fields', array($this, 'remove_required_from_name_fields' ) );
+		add_filter( 'woocommerce_billing_fields', array($this, 'remove_required_from_email_field' ) );
+		
 		add_action( 'woocommerce_customer_save_address', array($this, 'customer_save_address'), 10, 2);
 		add_action( 'woocommerce_companies_save_address', array($this, 'company_save_address'), 10, 3);
 		
@@ -29,13 +32,42 @@ class WC_Companies_Form_Handler extends WC_Form_Handler {
 		
 	}
 	
+	public function remove_required_from_name_fields($fields) {
+		
+		global $wp;
+
+		if( ! empty( $wp->query_vars['view-address'] ) ) {
+		
+			$fields['first_name']['required'] = false;
+			$fields['last_name']['required'] = false;
+			
+		}
+		
+		return $fields;
+		
+	}
+	
+	public function remove_required_from_email_field($fields) {
+		
+		global $wp;
+
+		if( ! empty( $wp->query_vars['view-address'] ) ) {
+		
+			$fields['billing_email']['required'] = false;
+			
+		}
+		
+		return $fields;
+		
+	}
+	
 	public function customer_save_address($user_id, $load_address) {
 		
 		global $wp;
 		
 		$customer = get_user_by('id', $user_id);
 		
-		$address = isset( $wp->query_vars['address_id'] ) ? wc_get_address($wp->query_vars['address_id']) : ( $customer->{ 'primary_' . $load_address . '_address' } && get_post($customer->{ 'primary_' . $load_address . '_address' }) ? wc_get_address($customer->{ 'primary_' . $load_address . '_address' }) : new WC_Address());
+		$address = isset( $wp->query_vars['view-address'] ) ? wc_get_address($wp->query_vars['view-address']) : ( $customer->{ 'primary_' . $load_address . '_address' } && get_post($customer->{ 'primary_' . $load_address . '_address' }) ? wc_get_address($customer->{ 'primary_' . $load_address . '_address' }) : new WC_Address());
 			
 		foreach ( WC()->countries->get_address_fields( esc_attr( $_POST[ $load_address . '_country' ] ), $load_address . '_' ) as $key => $field ) {
 			
@@ -50,6 +82,10 @@ class WC_Companies_Form_Handler extends WC_Form_Handler {
 		$address->save();
 		
 		do_action( 'woocommerce_companies_save_address', $user_id, $address->id, $load_address );
+		
+		wp_safe_redirect( wc_get_endpoint_url( 'addresses', '', wc_get_page_permalink( 'myaccount' ) ) );
+		
+		exit();
 		
 	}
 	
